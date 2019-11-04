7a228b4274c0aa0bbbc1ceee12af48a70e8205a4
nanjizal <nanjizal@users.noreply.github.com>
2019-11-04 01:25:31 +0000

    Create Int16Flat3.hx

---
+++ src/flat/Int16Flat3.hx
@@ -0,0 +1,55 @@
+package flat;
+import haxe.io.Int16Array;
+// arr[0] defines pos the position, pos is used to get items
+abstract Int16Flat3( Int16Array ) {
+    
+    public inline function new( len: Int ){
+        this = new Int16Array( len );
+        this[0] = 0;
+    }
+    
+    public var length( get, never ): Int;
+    inline function get_length(): Int {
+        var l = this.length - 1;
+        return ( l < 0 )? 0: Math.ceil( l / 3 );
+    }
+    public var index( get, set ): Int;
+    inline function get_index(): Int {
+        return Std.int( pos );
+    }
+    inline function set_index( id: Int ): Int {
+        pos = id;
+        return id;
+    }
+    public var pos( get, set ): Int;
+    inline function get_pos(): Int {
+        return this[ 0 ];
+    }
+    inline function set_pos( pos_: Int ): Int {
+        this[ 0 ] = pos_;
+        return pos_;
+    }
+    public inline function hasNext() return pos < length;
+    public inline function next(): Int {
+        pos = pos + 1.;
+        return pos;
+    }
+    @:op([])
+    //@:arrayAccess
+    public inline function readItem( k: Int ): Int {
+      return this[ index*3 + k ];
+    }
+    @:op([])
+    //@:arrayAccess
+    public inline function writeItem( k: Int, v: Int ): Int {
+        this[ index*3 + k ] = v;
+        return v;
+    }
+    
+    @:op(A++) public inline function increment() {
+        return next();
+    }
+    public inline function getArray(): Int16Array {
+        return this.subarray( 1, this.length - 1 );
+    }
+}
